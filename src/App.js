22fadc199fd8cf363f55139814df55db591b583f
Michael Sergio <mikeserg@gmail.com>
2018-06-27 21:23:01 -0400

    Extracted Navigation. Added live data fetch.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { HashRouter as Router,
          Route, Link, Switch, NavLink} from 'react-router-dom'
 import cn from 'classnames/bind';
 import FlipMove from 'react-flip-move';
+import { Navigation } from './Navigation.js';
 
 
 /*
@@ -16,17 +17,6 @@ import FlipMove from 'react-flip-move';
  * Add sort order. animations: joshwcomeau/react-flip-move 
  * Add Agenda view for starred.
  */
-
-function EventItem(props) { 
-    const name = props.name;
-    const source = props.source;
-    const dropdownClasses = cn("dropdown-item", {
-        "event--active": name === source,
-    });
-    return (
-        <li className={dropdownClasses} onClick={props.changeEvent}>{name}</li>
-    );
-}
 
 function getFilters(value) {
       if (value === "LARP") return ['Q', 'L'];
@@ -40,43 +30,6 @@ function getFilters(value) {
       else if (value === "Pencil Puzzles") return ['P'];
       else if (value === "Special Events & Panels") return ['S'];
       else return [];
-}
-
-function Navigation(props) {
-    const source = props.source;
-    const changeEvent = props.changeEvent;
-    const searchChange = props.searchChange;
-    const searchFilter = props.searchFilter;
-
-    const events = ["Dex21", "Dex20"].map((x) => {
-        return (<EventItem key={x} name={x} source={source} 
-            changeEvent={changeEvent} />)
-    });
-    return (
-        <ul className="navigation" role="navigation">
-            <li><NavLink to="/" exact activeClassName="navigation--selected">List</NavLink></li>
-            <li><NavLink to="/people" activeClassName="navigation--selected">People</NavLink></li>
-            <li><NavLink to="/system" activeClassName="navigation--selected">Systems</NavLink></li>
-            <li><NavLink to="/starred" exact activeClassName="navigation--selected">Starred</NavLink></li>
-            <li><NavLink to="/email" activeClassName="navigation--selected">Email</NavLink></li>
-            <li><NavLink to="/info" exact activeClassName="navigation--selected">Info</NavLink></li>
-            <li className="dropdown show">
-                <div className="dropdown-toggle" data-toggle="dropdown" 
-                    id="dropdownMenuLink"
-                    aria-haspopup="true" aria-expanded="false">Events</div>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <ul>
-                        {events}
-                    </ul>
-                </div>
-            </li>
-            <li className="navigation__search"> <form>
-                    <input onChange={searchChange}
-                        placeholder="Search" value={searchFilter} />
-                </form>
-            </li>
-        </ul>
-    );
 }
 
 function GridButtons(props) {
@@ -370,26 +323,10 @@ class App extends Component {
             searchFilter: "",
         }
     }
-    fetchLive() {
-        // const url = "https://www.dexposure.com/d21larp.html";
-        // const encodedUrl = encodeURIComponent(url);
-        // const coUrl = `http://allorigins.me/get?url=${encodedUrl}`;
-        /*
-        fetch(coUrl)
-          .then((response) => response.json())
-          .then((json) => {
-              const page = json.contents;
-              const parser = new DOMParser();
-              const doc = parser.parseFromString(page, "text/html");
-              // each p tag turns into a listing object or null
-              // { id: "L005", system: "Wayfinder", title: "The Hedge",
-              //   byline: "", presentedBy: "", 
-
-              this.setState({
-                  entries: json.contents,
-              });
-          });
-          */
+    fetchLive(url) {
+        const encodedUrl = encodeURIComponent(url);
+        const coUrl = `http://allorigins.me/get?url=${encodedUrl}`;
+        return fetch(coUrl).then((response) => response.json())
     }
     domParse(json) {
         const parser = new DOMParser();
@@ -402,8 +339,13 @@ class App extends Component {
         return entries;
     }
   fromSource(src) {
-      if (src === "D21-live") {
-          ;
+      if (src === "Dex21-live") {
+        const url = "https://www.dexposure.com/d21larp.html";
+        this.fetchLive(url).then((json) => {
+          this.setState({
+            entries: this.domParse(json),
+          });
+        });
       }
       else if (src === "Dex21") {
           this.setState({
---
+++ src/Navigation.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import cn from 'classnames/bind';
+import { HashRouter as Router, 
+    Route, Link, Switch, NavLink} from 'react-router-dom'
+
+function Navigation(props) {
+    const source = props.source;
+    const changeEvent = props.changeEvent;
+    const searchChange = props.searchChange;
+    const searchFilter = props.searchFilter;
+
+    const events = ["Dex21", "Dex20", "Dex21-live"].map((x) => {
+        return (<EventItem key={x} name={x} source={source} 
+            changeEvent={changeEvent} />)
+    });
+    return (
+        <ul className="navigation" role="navigation">
+            <li><NavLink to="/" exact activeClassName="navigation--selected">List</NavLink></li>
+            <li><NavLink to="/people" activeClassName="navigation--selected">People</NavLink></li>
+            <li><NavLink to="/system" activeClassName="navigation--selected">Systems</NavLink></li>
+            <li><NavLink to="/starred" exact activeClassName="navigation--selected">Starred</NavLink></li>
+            <li><NavLink to="/email" activeClassName="navigation--selected">Email</NavLink></li>
+            <li><NavLink to="/info" exact activeClassName="navigation--selected">Info</NavLink></li>
+            <li className="dropdown show">
+                <div className="dropdown-toggle" data-toggle="dropdown" 
+                    id="dropdownMenuLink"
+                    aria-haspopup="true" aria-expanded="false">Events</div>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <ul>
+                        {events}
+                    </ul>
+                </div>
+            </li>
+            <li className="navigation__search"> <form>
+                    <input onChange={searchChange}
+                        placeholder="Search" value={searchFilter} />
+                </form>
+            </li>
+        </ul>
+    );
+}
+
+function EventItem(props) { 
+    const name = props.name;
+    const source = props.source;
+    const dropdownClasses = cn("dropdown-item", {
+        "event--active": name === source,
+    });
+    return (
+        <li className={dropdownClasses} onClick={props.changeEvent}>{name}</li>
+    );
+}
+
+export { Navigation } ;
