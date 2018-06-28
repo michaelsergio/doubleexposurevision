ee7e6dafc45bd2624b085ec5b29cabc81ae67864
Michael Sergio <mikeserg@gmail.com>
2018-06-27 22:40:07 -0400

    Added sorting and database saving.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,27 @@ import { Navigation } from './Navigation.js';
  * Add Agenda view for starred.
  */
 
+function entryToDateNum(listEntry) {
+    try {
+        const d = listEntry.day;
+        let dayNum = 0;
+        if (d.indexOf("Mon") !== -1) dayNum = 1;
+        else if (d.indexOf("Tue") !== -1) dayNum = 2;
+        else if (d.indexOf("Wed") !== -1) dayNum = 3;
+        else if (d.indexOf("Thu") !== -1) dayNum = 4;
+        else if (d.indexOf("Fri") !== -1) dayNum = 5;
+        else if (d.indexOf("Sat") !== -1) dayNum = 6;
+        else if (d.indexOf("Sun") !== -1) dayNum = 7;
+
+        const timeStart = listEntry.time.split(" ")[0];
+        const mIndex = timeStart.indexOf("M");
+        const isPM = timeStart[mIndex - 1] === "P";
+        const time1200 = timeStart.substring(0, mIndex - 2).replace(":", "");
+        const time2400 = parseInt(time1200) + (isPM ? 2400 : 0) ;
+        return dayNum * 10000 + time2400;
+    } catch(error) { return 0; }
+}
+
 function getFilters(value) {
       if (value === "LARP") return ['Q', 'L'];
       else if (value === "NAGA") return ['G'];
@@ -87,31 +108,62 @@ function TypeList(props) {
   );
 }
 
-function StarredEntryList(props) {
-    const entries = props.entries;
-    const clickSave = props.clickSave;
-    const changeLayout = props.changeLayout;
-    const db = props.db;
-    const view = props.view;
-    const entryList = entries
-        .filter((le) => db[le.id] || false)
-        .map((le) => {
-            const saved = db[le.id] || false;
-            return (<Entry key={le.id} dict={le} saved={saved} view={view}
-                        clickSave={(e) => clickSave(e, le.id)} />
-            );
-        });
-    if (entryList.length === 0) {
-        return (
-            <div className="starred-entries__none">No Starred Entries.</div>
-        );
+class StarredEntryList extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {sortOrder: "time", };
     }
-    return (<div>
-        <GridButtons changeLayout={changeLayout} />
-        <FlipMove enterAnimation="fade" leaveAnimation="fade">
-            {entryList}
-        </FlipMove>
-    </div>);
+    changeSortOrder(e) {
+        this.setState({
+            sortOrder: e.target.value,
+        });
+    }
+    sort(strategy, entries) {
+        if (strategy === "time") {
+            return entries.sort((x,y) => entryToDateNum(x) - entryToDateNum(y));
+        }
+        else if (strategy === "name") {
+            return entries.sort((x,y) => x.name.localeCompare(y.name));
+        }
+        else if (strategy === "id") {
+            return entries.sort((x,y) => x.id.localeCompare(y.id));
+        }
+        return entries;
+    }
+    render() {
+        const entries = this.props.entries;
+        const clickSave = this.props.clickSave;
+        const changeLayout = this.props.changeLayout;
+        const db = this.props.db;
+        const view = this.props.view;
+        const sortOrder = this.state.sortOrder;
+        const entryList = this.sort(sortOrder, entries.
+            filter((le) => db[le.id] || false))
+            .map((le) => {
+                const saved = db[le.id] || false;
+                return (<Entry key={le.id} dict={le} saved={saved} view={view}
+                    clickSave={(e) => clickSave(e, le.id)} />
+                );
+            });
+        if (entryList.length === 0) {
+            return (
+                <div className="starred-entries__none">No Starred Entries.</div>
+            );
+        }
+        return (<div className="starred">
+            <GridButtons changeLayout={changeLayout} />
+            <select className="starred-entries__sorter"
+                onChange={(e) =>this.changeSortOrder(e)} id="" name="">
+                    <option value="none">No Sort Order</option>
+                    <option value="name">Name</option>
+                    <option value="id">ID</option>
+                    <option value="time" selected>Time</option>
+            </select>
+            <FlipMove enterAnimation="fade" leaveAnimation="fade">
+                {entryList}
+            </FlipMove>
+        </div>);
+    }
 }
 
 function EntryList(props) {
@@ -280,7 +332,7 @@ class Entry extends Component {
 
 function isSelected(title, filters) {
     const titleFilters = getFilters(title)
-    return titleFilters.length == filters.length && 
+    return titleFilters.length === filters.length && 
         titleFilters.every((x) => filters.includes(x));
 }
 
@@ -366,10 +418,49 @@ class App extends Component {
       });
       this.fromSource(value)
   }
+    hydrateStateWithLocalStorage() {
+        for (let key in this.state) {
+            if (localStorage.hasOwnProperty(key)) {
+                let value = localStorage.getItem(key);
+                try {
+                    value = JSON.parse(value);
+                    this.setState({ [key]: value });
+                } catch (e) {
+                    // handle empty string
+                    this.setState({ [key]: value });
+                }
+            }
+        }
+    }
+
+    saveStateToLocalStorage() {
+        for (let key in this.state) {
+            localStorage.setItem(key, JSON.stringify(this.state[key]));
+        }
+    }
+
   componentDidMount() {
+      this.hydrateStateWithLocalStorage();
+
       const src = this.state.source;
       this.fromSource(src);
+
+      // add event listener to save state to localStorage
+      // when user leaves/refreshes the page
+      window.addEventListener(
+          "beforeunload",
+          this.saveStateToLocalStorage.bind(this));
   }
+    componentWillUnmount() {
+        window.removeEventListener(
+            "beforeunload",
+            this.saveStateToLocalStorage.bind(this)
+        );
+
+        // saves if component has a chance to unmount
+        this.saveStateToLocalStorage();
+    }
+
   clickSave(event, id) {
     const db = this.state.db;
     const val = db[id] || false;
